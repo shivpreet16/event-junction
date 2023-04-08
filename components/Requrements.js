0c14c4fe783d6c04ed899668c988ce3d28ba5858
shivpreet <shivpreet16@gmail.com>
2023-04-08 12:38:25 +0530

    faculty and requierments

--- components/Requrements.js
+++ components/Requrements.js
@@ -1,11 +1,15 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 const Requrements = () => {
-  return (
-    <div>
-        <input type="text" name="" className='apply-input' placeholder='req' id="" />
-    </div>
-  )
+    const [val, setVal] = useState('')
+    return (
+        <div>
+            <input type="text" name="" className='apply-input' placeholder='req' id="" onChange={(newval) => {
+                setVal(newval)
+                console.log(newval.target.value)
+            }} />
+        </div>
+    )
 }
 
 export default Requrements
--- src/pages/faculty.js
+++ src/pages/faculty.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react'
+// faculty.js
+import React, { useEffect, useState } from 'react'
 import { getCookie } from 'cookies-next'
 import { useRouter } from 'next/router'
 import Calendar from '../../components/Calendar'
 import AllEvents from '../../components/AllEvents'
+import Profile from '../../components/Profile'
 
 export default function faculty() {
   const router = useRouter()
@@ -11,15 +13,54 @@ export default function faculty() {
       router.push('/')
     }
   }, [])
+  const [active, setactive] = useState('All Events')
+  const [email, setemail] = useState('')
+  const [error, seterror] = useState('no data')
+
+  const getUserDetails = async(e)=> {
+    e.preventDefault()
+    setactive('Profile')
+    const val = {
+      "cookie": getCookie('faculty_cookie')
+    }
+
+    const response = await fetch('api/getUser', {
+      method: 'POST',
+      body: JSON.stringify(val),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+
+    data = await response.json()
+    
+    if(data){
+      setemail(data.email)
+      console.log(data)
+      seterror('')
+    }
+    else{
+      seterror(data.error)
+    }
+  }
+
   return (
-    <div className='max-h-fit h-full lg:h-[100vh] w-full bg-[#ededed] flex flex-col lg:flex-row justify-between '>
+    <div className='max-h-fit h-full lg:h-[100vh] w-full bg-[#ededed] flex flex-col lg:flex-row justify-between gradient'>
     <div className='flex flex-col w-full lg:w-[70%] gap-2'>
-      <ul className='w-full flex justify-evenly mt-10'>
+      <ul className='w-full flex justify-evenly mt-10 nav'>
         <li>All Events</li>
-        <li>Your Requests</li>
-        <li>Profile</li>
+        <li onClick={(e)=>{
+          e.preventDefault()
+          setactive('Profile')}}>Your Requests</li>
+        <li onClick={getUserDetails} className='cursor-pointer'>Profile</li>
       </ul>
-      <div className='bg-white h-[80vh] bottom-0  m-5 rounded-[10px]'></div>
+      <Profile 
+      email={email} 
+      error={error}
+      setactive={setactive}/>
+      <div className='bg-white h-[80vh] bottom-0  m-5 rounded-[10px]'>
+        <Profile />
+      </div>
     </div>
     {/* <span>{message}</span> */}
     <div className='flex flex-col md:pb-4 lg:pb-0 md:flex-row lg:flex-col md:right-0 md:ml-10 lg:m-10 md:mt-3 mt-7 w-[100%] lg:w-1/4'>
