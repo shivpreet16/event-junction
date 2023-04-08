b4b7d30669b7c40c6ade9b198d9b7010e15a7abe
shivpreet <shivpreet16@gmail.com>
2023-04-08 12:41:00 +0530

    faculty

--- src/pages/faculty.js
+++ src/pages/faculty.js
@@ -47,23 +47,13 @@ export default function faculty() {
 
   return (
     <div className='max-h-fit h-full lg:h-[100vh] w-full bg-[#ededed] flex flex-col lg:flex-row justify-between gradient'>
-    <div className='max-h-fit h-full lg:h-[100vh] w-full bg-[#ededed] flex flex-col lg:flex-row justify-between gradient'>
     <div className='flex flex-col w-full lg:w-[70%] gap-2'>
       <ul className='w-full flex justify-evenly mt-10 nav'>
-      <ul className='w-full flex justify-evenly mt-10 nav'>
         <li>All Events</li>
-        <li onClick={(e)=>{
-          e.preventDefault()
-          setactive('Profile')}}>Your Requests</li>
-        <li onClick={getUserDetails} className='cursor-pointer'>Profile</li>
+        <li>Your Requests</li>
+        <li>Profile</li>
       </ul>
-      <Profile 
-      email={email} 
-      error={error}
-      setactive={setactive}/>
-      <div className='bg-white h-[80vh] bottom-0  m-5 rounded-[10px]'>
-        <Profile />
-      </div>
+      <div className='bg-white h-[80vh] bottom-0  m-5 rounded-[10px]'></div>
     </div>
     {/* <span>{message}</span> */}
     <div className='flex flex-col md:pb-4 lg:pb-0 md:flex-row lg:flex-col md:right-0 md:ml-10 lg:m-10 md:mt-3 mt-7 w-[100%] lg:w-1/4'>
