4db773a92c4e95307ad539acde29cb544ddce51b
shivpreet <shivpreet16@gmail.com>
2023-04-17 16:21:49 +0530

    added community

--- components/Community.js
+++ components/Community.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Community = () => {
+const Community = ({ email, name, YOE, dept, active }) => {
   return (
-    <div>Community</div>
+    <div
+      className={`bg-[#edededed] h-full w-80% ml-10 flex flex-col mb-10 rounded-[10px] text-[#6d8667] justify-center items-center relative ${
+        active === "community" ? "block" : "hidden"
+      }`}
+    >
+        
+    </div>
   )
 }
 
